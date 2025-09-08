1503912210bfdb8b1cb03277b00c22d4bfe92ab4
tuananhhust05 <tuananhhust05@gmail.com>
2025-09-08 23:49:47 +0700

    update

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ async def handler(websocket):
             print("Saved output.wav")
 
 async def main():
-    async with websockets.serve(handler, "0.0.0.0", 8765, path="/media"):
-        print("WebSocket server listening on ws://0.0.0.0:8765/media")
+    async with websockets.serve(handler, "0.0.0.0", 8765):
+        print("WebSocket server listening on ws://0.0.0.0:8765")
         await asyncio.Future()
 
 asyncio.run(main())
